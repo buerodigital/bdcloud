e60b44e72be8ed400e4451328969326abe973cf4
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-01-23 21:20:14 +0100

    Update bdcloud.sh

--- 01_Host/bdcloud.sh
+++ 01_Host/bdcloud.sh
@@ -7,3 +7,11 @@ cp -f /bdcloud/02_Proxy_local/default /bdcloud/conf_02_Proxy/nginx/site-confs/de
 cp -f /bdcloud/02_Proxy_local/proxy.conf /bdcloud/conf_02_Proxy/nginx/proxy.conf
 mkdir /bdcloud/conf_02_Proxy/nginx/proxy-confs
 cp -f /bdcloud/02_Proxy_local/heimdall.subfolder.conf /bdcloud/conf_02_Proxy/nginx/proxy-confs/heimdall.subfolder.conf
+
+# Installation 03_Pihole
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+sudo cp /bdcloud/03_Pihole/resolv.conf /etc/resolv.conf
+docker-compose -f /bdcloud/03_Pihole/docker-compose.yml up --no-start
+cp -f /bdcloud/03_Pihole/pihole.subfolder.conf /bdcloud/conf_02_Proxy/nginx/proxy-confs/heimdall.subfolder.conf
+
