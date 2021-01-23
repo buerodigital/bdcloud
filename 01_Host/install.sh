cad4a359e87ac489ddc7cfc175e950037e3e1602
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-01-23 20:06:43 +0100

    Update install.sh

--- 01_Host/install.sh
+++ 01_Host/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Systemupdate und Hostnamen
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
 
 # Basisprogramme installieren
 git, docker, docker-compose, openssl, open ssh
@@ -9,6 +12,11 @@ git, docker, docker-compose, openssl, open ssh
 "Root" User der Dockergruppen hinzufügen
 
 # Git Repo klonen und Verzeichnisstruktur erstellen
+cd ~
+git clone https://github.com/buerodigital/bdcloud.git
+docker system prune -a --volumes
+docker network create proxy
+docker-compose -f ~/bdcloud/02_Proxy_local/docker-compose.yml up --no-start
 
 
 # Bash Menü für User, etc
