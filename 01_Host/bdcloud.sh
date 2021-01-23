f74e9a6c628211481ecd822322a0dda1b8bf4fb1
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-01-23 21:07:01 +0100

    Create bdcloud.sh

---
+++ 01_Host/bdcloud.sh
@@ -0,0 +1,9 @@
+
+
+# Installation 02_Proxy_local
+docker network create proxy
+docker-compose -f /bdcloud/02_Proxy_local/docker-compose.yml up --no-start
+cp -f /bdcloud/02_Proxy_local/default /bdcloud/conf_02_Proxy/nginx/site-confs/default
+cp -f /bdcloud/02_Proxy_local/proxy.conf /bdcloud/conf_02_Proxy/nginx/proxy.conf
+mkdir /bdcloud/conf_02_Proxy/nginx/proxy-confs
+cp -f /bdcloud/02_Proxy_local/heimdall.subfolder.conf /bdcloud/conf_02_Proxy/nginx/proxy-confs/heimdall.subfolder.conf
