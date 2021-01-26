b59de869efe5d38c3da10d755ba24ee14f222c75
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-01-26 21:03:10 +0100

    Create docker.sh

---
+++ 01_Host/docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker stop $(sudo docker ps -a -q)
+docker rm $(sudo docker ps -a -q)
+docker system prune -a --volumes
+umount /var/lib/docker/volumes
+
+
+cd /
+
+sudo rm -Rf /bdcloud
+
+sudo git clone https://github.com/buerodigital/bdcloud.git
+mkdir /bdcloud/_volumes
+sudo chown -R 1000:1000 /bdcloud
+sudo chmod -R +x /bdcloud
+sudo mount --bind /bdcloud/_volumes /var/lib/docker/volumes
+
+
+cd /bdcloud/01_Host
+./bdcloud.sh
