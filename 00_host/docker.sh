a0301b84e7c35a0e8b25d3fb7ec3f09f19fa3f0c
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-01-31 14:32:24 +0100

    Update docker.sh

--- 00_host/docker.sh
+++ 00_host/docker.sh
@@ -5,13 +5,12 @@ docker rm $(sudo docker ps -a -q)
 docker system prune -a --volumes
 umount /var/lib/docker/volumes
 
-
 cd /
-
 sudo rm -Rf /bdcloud
 
 sudo git clone https://github.com/buerodigital/bdcloud.git
-mkdir /bdcloud/_volumes
+sudo mkdir /bdcloud/_volumes
+sudo cp -R /var/lib/docker/volumes/* /bdcloud/_volumes/
 sudo chown -R 1000:1000 /bdcloud
 sudo chmod -R +x /bdcloud
 sudo mount --bind /bdcloud/_volumes /var/lib/docker/volumes
