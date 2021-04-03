9fa2ca1096bb18272499bc469b9178b03eb4caa6
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-04-03 20:18:43 +0200

    Update install.sh

--- 00_host/install.sh
+++ 00_host/install.sh
@@ -14,6 +14,9 @@ sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s) -$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+sudo systemctl start docker
+sudo systemctl enable docker
+
 # Aufräumen
 sudo apt-get --assume-yes clean
 sudo apt-get --assume-yes autoremove
