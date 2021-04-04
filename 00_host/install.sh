05b82f7395d5317c7f5ad34fd129f790222dad1d
buerodigital <77230151+buerodigital@users.noreply.github.com>
2021-04-04 13:35:26 +0200

    Update install.sh

--- 00_host/install.sh
+++ 00_host/install.sh
@@ -11,7 +11,7 @@ sudo apt-get --assume-yes install git openssl openssh-server apt-transport-https
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s) -$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo systemctl start docker
